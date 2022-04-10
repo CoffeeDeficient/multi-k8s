26bd3dfdbfe783c73e442ad63a3e73e3f8c4ccfc
coffeedeficient <coffeedeficient@gmail.com>
2022-04-10 08:31:12 -0600

    Added client

---
+++ client/src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+/*
+test('Home link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+*/
+
+test('test2', () => {});
