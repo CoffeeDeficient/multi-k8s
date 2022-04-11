09bd3352430166ac443df28a3dbeccf95bd0b89a
coffeedeficient <coffeedeficient@gmail.com>
2022-04-10 21:27:41 -0600

    updated title

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator v2.0
+            Fib Calculator v2.1 NOW with More Kubernetes!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
